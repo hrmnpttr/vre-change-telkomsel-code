4a2c1cbe63c0d178ba536cc46a8d7618881a96f0
Ricky CK <hrmnpttr@gmail.com>
2020-08-17 00:12:40 +0700

    Update backup01.bat

--- backup01.bat
+++ backup01.bat
@@ -2,7 +2,7 @@ set yy=%date:~-4%
 set mm=%date:~-7,2%
 set dd=%date:~-10,2%
 set MYDATE=%yy%%mm%%dd%
-cd c:\Users\u2\Documents\dumps
+cd c:\dumps
 IF NOT EXIST %yy%%mm% MKDIR %yy%%mm%
 cd %yy%%mm%
-mysqldump --single-transaction --quick -u root -h 192.168.1.2 refill_mlm | gzip > dump_%MYDATE%_0100.sql.gz
+mysqldump --single-transaction --quick -u root -h 192.168.1.1 refill_mlm | gzip > dump_%MYDATE%_0100.sql.gz
